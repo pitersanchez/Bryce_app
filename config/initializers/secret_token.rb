91834a845ad4d8e9dafd3c3116e22bca43a293eb
unknown <Bryce@.(none)>
2011-06-04 22:56:04 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BryceApp::Application.config.secret_token = 'c924ab498c5d9b2084615d28bfcf86c1a6f78f90b5b949159a202ce3417b47a6bbd2541e53eebdb0817b866e151da55563d1b3d697f60e7106a630ce1e05d9d6'
